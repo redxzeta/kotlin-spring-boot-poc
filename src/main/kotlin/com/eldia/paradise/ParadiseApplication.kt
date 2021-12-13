36e27238562a199af0f042769633ff586e7f8a8b
redxzeta <xzetanzero@gmail.com>
2021-12-12 17:14:59 -0800

    kotlin poc

---
+++ src/main/kotlin/com/eldia/paradise/ParadiseApplication.kt
@@ -0,0 +1,11 @@
+package com.eldia.paradise
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ParadiseApplication
+
+fun main(args: Array<String>) {
+	runApplication<ParadiseApplication>(*args)
+}
---
+++ src/test/kotlin/com/eldia/paradise/ParadiseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.eldia.paradise
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ParadiseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
